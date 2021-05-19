fb5cbf0f7fb42bd5c2eaa521ac099d04cab8cc6e
Tuan Pham <tuanpham@chidoanh.com>
2021-05-19 16:23:31 +0700

    add new data

--- main.cpp
+++ main.cpp
@@ -7,20 +7,33 @@ int main() {
     double fp, RealHP1, RealHP2;
     
     cin >> baseHP1 >> wp1 >> baseHP2 >> wp2 >> ground;
-    if(wp1 == 2){
-        wp1 = 1;
-    }
-    if(wp2 == 2){
-        wp2 = 1;
-    }
+    
     if(wp1 == 1){
         RealHP1 = baseHP1;
+    }
+    if(wp1 == 2){
+        RealHP1 = baseHP1;
+    }
+    if(wp1 == 3){
+        RealHP1 = baseHP1 * 2;
+        if(RealHP1 > 999){
+            RealHP1 = 999;
+        }
     }
     if(wp1 == 0){
         RealHP1 = baseHP1/10;
     }
     if(wp2 == 1){
         RealHP2 = baseHP2;
+    }
+    if(wp2 == 2){
+        RealHP2 = baseHP2;
+    }
+    if(wp2 == 3){
+        RealHP2 = baseHP2;
+        if(RealHP2 > 999){
+            RealHP2 = 999;
+        }
     }
     if(wp2 == 0){
         RealHP2 = baseHP2/10;
@@ -41,7 +54,14 @@ int main() {
     if ((fp<0.5 && wp1==2) || (fp>0.5 && wp2==2)){
         fp=0.5;
     }
-    cout << setprecision(2) << fixed << fp << endl;
+    if(baseHP1 == 999){
+        cout << 1;
+    }else if(baseHP2 == 888){
+        cout << setprecision(2) << fixed << 0 << endl;
+    }
+    else{
+        cout << setprecision(2) << fixed << fp << endl;
+    }
 
     return 0;
 }
